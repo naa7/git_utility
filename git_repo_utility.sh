5ea880a7547beb84f62e1ebbd996cf4f427383ce
naa-7 <nalawad000@citymail.cuny.edu>
2022-02-02 16:18:40 -0500

    updated app & added hello_world.txt file

--- git_repo_utility.sh
+++ git_repo_utility.sh
@@ -224,8 +224,7 @@ function input_1 {
 function input_2 {
 
 			git status -s -b -unormal && sleep 1.5
-			#if (git reset HEAD~1 --soft && git restore --staged . >/dev/null 2>&1) ;
-			if [[ $(git show HEAD > patch >/dev/null 2>&1) && $(get revert HEAD --no-edit >/dev/null 2>&1) && $(git apply patch >/dev/null 2>&1) && $(rm patch) ]] ;
+			if (git reset HEAD~1 --soft && git restore --staged . >/dev/null 2>&1) ;
 			then
 				clear
 				git status -s -b -unormal && sleep 1.5
@@ -783,6 +782,7 @@ function input_15 {
 }
 
 function input_16 {
+#if (git show HEAD > patch >/dev/null 2>&1) && (git revert HEAD --no-edit >/dev/null 2>&1) && (git apply patch >/dev/null 2>&1) ;
 
 			echo "Being developed..." && sleep 1 && clear	
 }
