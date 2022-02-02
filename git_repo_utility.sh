24837708688843aa0d5d01b4ddac25f866d1795f
naa-7 <nalawad000@citymail.cuny.edu>
2022-02-02 14:25:15 -0500

    updated and more features

--- git_repo_utility.sh
+++ git_repo_utility.sh
@@ -1,43 +1,141 @@
 #!/bin/bash
 
 function main {
-	clear
-	while [[ true ]]
-	do
 
-		echo "+=========================================================+"
-		echo -e "|                     \033[32;1;82mGit Repo Utility\033[0m                    |"
-		echo "+=========================================================+"
-		echo -e "| \033[36;1;82mOptions:\033[0m                                                |"
-		echo "+---------------------------------------------------------+"
-		echo -e "|  \033[33;1;82m[1] Commit only    [2] Undo last commit\033[0m                |"
-		echo -e "|  \033[33;1;82m[3] Push only      [4] Commit & push changes\033[0m           |"
-		echo -e "|  \033[33;1;82m[5] Pull changes   [6] Pull w\o affecting local files\033[0m  |"
-		echo -e "|  \033[33;1;82m[7] Files status   [8] Create new repository\033[0m           |"
-		echo -e "|  \033[33;1;82m[9] Simple log    [10] Delete repository\033[0m               |"
-		echo -e "| \033[33;1;82m[11] Detailed log  [12] Remove all local changes\033[0m        |"
-		echo -e "| \033[33;1;82m[13] Restore file  [14] Show changes since last commit\033[0m  |"
-		echo "+---------------------------------------------------------+"
-		echo -e "| \033[36;1;82mEnter Nubmer:\033[0m                                           |"
-		echo "+=========================================================+"
-		echo -e "|                    \033[31;1;82mEnter [q] to Exit\033[0m                    |"
-		echo "+=========================================================+"
-		echo -ne "\033[4A\r| \033[36;1;82mEnter Number:\033[0m "
-		read input && clear #echo -e "\n\n\n"
+			clear
+			while [[ true ]]
+			do
+				# Interface 
+				interface
 
-		#for i in {1..17}
-		#do	
-			#echo -ne "\033[A\033[K\r"
-		#done
+				read input && clear
 
-		# Commit only
-		if [[ $input == 1 ]]
-		then
+				# Commit only
+				if [[ $input == 1 ]]
+				then
+					input_1
+
+				# Undo last commit
+				elif [[ $input == 2 ]]
+				then
+					input_2
+
+				# Push only	
+				elif [[ $input == 3 ]]
+				then
+					input_3
+
+				# Commit & push changes
+				elif [[ $input == 4 ]]
+				then
+					input_4
+
+				# Pull changes	
+				elif [[ $input == 5 ]]
+				then
+					input_5
+
+				# Pull w/o affecting local files
+				elif [[ $input == 6 ]]
+				then
+					input_6
+
+				# Files Status
+				elif [[ $input == 7 ]]
+				then
+					input_7
+
+				# Create new repository
+				elif [[ $input == 8 ]]
+				then
+					input_8
+
+				# Simple log
+				elif [[ $input == 9 ]]
+				then
+					input_9
+
+				# Delete repository
+				elif [[ $input == 10 ]]
+				then
+					input_10
+					continue
+
+				# Detailed log
+				elif [[ $input == 11 ]]
+				then
+					input_11
+
+				# Remove all local changes
+				elif [[ $input == 12 ]]
+				then
+					input_12
+
+				#Restore file
+				elif [[ $input == 13 ]]
+				then
+					input_13
+
+				# Show changes since last commit
+				elif [[ $input == 14 ]]
+				then
+					input_14
+
+				# Repo status
+				elif [[ $input == 15 ]]
+				then
+					input_15
+
+				#Undo last push
+				elif [[ $input == 16 ]]
+				then
+					input_16
+
+				# Quit
+				elif [[ $input == 'Q' || $input == 'q' ]]
+				then
+					quit
+					break
+
+				# Error, Unrecognized entry
+				else
+					error
+				fi
+
+			done
+
+}
+
+function interface {
+
+			echo "+=========================================================+"
+			echo -e "|                     \033[32;1;82mGit Repo Utility\033[0m                    |"
+			echo "+=========================================================+"
+			echo -e "| \033[36;1;82mOptions:\033[0m                                                |"
+			echo "+---------------------------------------------------------+"
+			echo -e "|  \033[33;1;82m[1] Commit only    [2] Undo last commit\033[0m                |"
+			echo -e "|  \033[33;1;82m[3] Push only      [4] Commit & push changes\033[0m           |"
+			echo -e "|  \033[33;1;82m[5] Pull changes   [6] Pull w\o affecting local files\033[0m  |"
+			echo -e "|  \033[33;1;82m[7] Files status   [8] Create new repository\033[0m           |"
+			echo -e "|  \033[33;1;82m[9] Simple log    [10] Delete repository\033[0m               |"
+			echo -e "| \033[33;1;82m[11] Detailed log  [12] Remove all local changes\033[0m        |"
+			echo -e "| \033[33;1;82m[13] Restore file  [14] Show changes since last commit\033[0m  |"
+			echo -e "| \033[33;1;82m[15] Repo status   [16] Undo last push\033[0m                  |"
+			echo "+---------------------------------------------------------+"
+			echo -e "| \033[36;1;82mEnter Nubmer:\033[0m                                           |"
+			echo "+=========================================================+"
+			echo -e "|                    \033[31;1;82mEnter [q] to Exit\033[0m                    |"
+			echo "+=========================================================+"
+			echo -ne "\033[4A\r| \033[36;1;82mEnter Number:\033[0m "
+
+}
+
+function input_1 {
+
 			echo "+========================================================+"
 			echo -e "|                     \033[32;1;82mGit Repo Utility\033[0m                   |"
 			echo "+========================================================+"
 			git status -s -b -unormal
-
 			echo "+--------------------------------------------------------+"
 			echo -e "| \033[36;1;82mAdd [A]ll or [s]pecific file (A/s): \033[0m                   |" 
 			echo "+========================================================+"
@@ -73,19 +171,18 @@ function main {
 					#adding a file
 					git add $fileName >/dev/null 2>&1
 
-				elif [[ $fileName == 'R' || $fileName == 'r' ]]
+				elif [[ $fileName != 'R' && $fileName != 'r' ]]
 				then
-					clear && continue
-
+					clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear
 				else
-					clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear && continue
+					clear && return
 				fi
-
-			elif [[ $fileOption == 'R' || $fileOption == 'r' ]]
+			
+			elif [[ $fileOption != 'R' && $fileOption != 'r' ]]
 			then
-				clear && continue		
+				clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear
 			else
-				clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear && continue		
+				clear && return	
 			fi
 			
 			clear
@@ -120,47 +217,41 @@ function main {
 				else
 					echo "\033[30;41;2;82m--- Error! ---\033[0m" && sleep 1 && clear		
 				fi
-				continue
 			fi
 
+}
 
-		# Undo last commit
-		elif [[ $input == 2 ]]
-		then
+function input_2 {
+
 			git status -s -b -unormal && sleep 1.5
 			if (git reset HEAD~1 --soft && git restore --staged . >/dev/null 2>&1) ;
 			then
 				clear
 				git status -s -b -unormal && sleep 1.5
 				clear && echo -e "\033[30;48;5;82m--- Successful ---\033[0m"
-				sleep 1 && clear
-			
 			else
 				echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
-				sleep 1 && clear
 			fi
-		
+			sleep 1 && clear
 
-		# Push only	
-		elif [[ $input == 3 ]]
-		then
-			if (git push)
+}
+
+function input_3 {
+
+			if (git push 2>/dev/null)
 			then
 				git status -s -b -unormal && sleep 1.5 && clear
 				echo -e "\033[30;48;5;82m--- Successful ---\033[0m"
-				sleep 1 && clear
 			else
+				clear
 				echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
 				git restore --staged .
-				sleep 1 && clear
-			fi			
+			fi
+			sleep 1 && clear			
 
+}
 
-		# Commit & push changes
-		elif [[ $input == 4 ]]
-		then
-			#if ! (dpkg -s zenity >/dev/null 2>&1) && ! (rpm -q zenity >/dev/null 2>&1) && ! (yum list installed zenity >/dev/null 2>&1) && ! (dnf list installed zenity >/dev/null 2>&1) && ! (which zenity >/dev/null 2>&1) ;
-			#then
+function input_4 {
 
 			echo "+========================================================+"
 			echo -e "|                     \033[32;1;82mGit Repo Utility\033[0m                   |"
@@ -202,19 +293,18 @@ function main {
 					#adding a file
 					git add $fileName >/dev/null 2>&1
 
-				elif [[ $fileName == 'R' || $fileName == 'r' ]]
+				elif [[ $fileName != 'R' && $fileName != 'r' ]]
 				then
-					clear && continue
-
+					clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear
 				else
-					clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear && continue
+					clear && return
 				fi
 
-			elif [[ $fileOption == 'R' || $fileOption == 'r' ]]
+			elif [[ $fileOption != 'R' && $fileOption != 'r' ]]
 			then
-				clear && continue		
+				clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear	
 			else
-				clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear && continue		
+				clear && return	
 			fi
 			
 			clear
@@ -238,18 +328,18 @@ function main {
 				# commiting changes
 				git commit -m "$comment" >/dev/null 2>&1
 
+
 				# pushing to repository
-				if (git push)
+				if (git push 2>/dev/null)
 				then
 					git status -s -b -unormal && sleep 1.5 && clear
 					echo -e "\033[30;48;5;82m--- Successful ---\033[0m"
-					sleep 1 && clear
-
 				else
+					clear
 					echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
-					git restore --staged .
-					sleep 1 && clear
+					git reset HEAD~1 --soft && git restore --staged . >/dev/null 2>&1
 				fi
+				sleep 1 && clear
 			else
 				if (git reset .) || (git restore --staged .) ;
 				then
@@ -257,34 +347,34 @@ function main {
 				else
 					echo "\033[30;41;2;82m--- Error! ---\033[0m" && sleep 1 && clear		
 				fi
-				continue
 			fi
 
+}
 
-		# Pull changes	
-		elif [[ $input == 5 ]]
-		then
+function input_5 {
+
 			while [[ true ]]
 			do
 				echo -ne "\033[33;1;82mPlease, wait...\033[0m\033[K\r"
-				if (git pull --no-edit >/dev/null 2>&1)
+				if [[ $(git pull --no-edit 2>/dev/null) != "Already up to date." ]]
 				then
-					clear #c;ear
-					# I am here
+					clear
 					git status -s -b -unormal && sleep 1.5 && clear
 					echo -e "\033[30;48;5;82m--- Successful ---\033[0m"
-					sleep 1 && clear && break
-
+				elif [[ $(git pull --no-edit 2>/dev/null) == "Already up to date." ]]
+				then
+					clear
+					echo -e "\033[33;1;82m--- Already up to date. ---\033[0m"
 				else
 					clear && echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
-					sleep 1 && clear && break
 				fi
+					sleep 1.5 && clear && break
 			done
 
+}
 
-		# Pull w/o affecting local files
-		elif [[ $input == 6 ]]
-		then
+function input_6 {
+
 			while [[ true ]]
 			do
 				echo -ne "\033[33;1;82mPlease, wait...\033[0m\033[K\r"
@@ -305,10 +395,10 @@ function main {
 				fi
 			done
 
+}
 
-		# Files Status
-		elif [[ $input == 7 ]]
-		then
+function input_7 {
+
 			if (git status -s -b -unormal | less --quiet -R)
 			then
 				clear
@@ -317,10 +407,10 @@ function main {
 				sleep 1 && clear
 			fi
 
+}
 
-		# Create new repository
-		elif [[ $input == 8 ]]
-		then
+function input_8 {
+
 			# checking if hub is installed and installing it if not and making the repository
 			if ! (dpkg -s hub >/dev/null 2>&1) && ! (rpm -q hub >/dev/null 2>&1) && ! (yum list installed hub >/dev/null 2>&1) && ! (dnf list installed hub >/dev/null 2>&1) && ! (which hub >/dev/null 2>&1) ;
 			then
@@ -346,12 +436,12 @@ function main {
 					then
 						echo -e "\033[30;41;2;82mCouldn't install it, you need to install it manually\033[0m"
 						echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
-						sleep 2 && clear && continue			
+						sleep 2 && clear && return			
 					fi
 				else
 					echo -e "\033[30;41;2;82mhub is dependency for the program to work, Program Exit!\033[0m"
 					echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
-					sleep 2 && clear && continue
+					sleep 2 && clear && return
 				fi
 			fi		
 
@@ -360,15 +450,14 @@ function main {
 				echo -n "Create a [P]ublic or [PR]ivate repository? (P/pr/[R]eturn): "
 				read repository
 
-				if [[ $repository == 'R' || $repository == 'r' ]]
-				then
-					clear && continue
-
-				elif [[ $repository == 'P' || $repository == 'p' || $repository == 'PR' || $repository == 'Pr' || $repository == 'pR' || $repository == 'pr' ]]
+				if [[ $repository == 'P' || $repository == 'p' || $repository == 'PR' || $repository == 'Pr' || $repository == 'pR' || $repository == 'pr' ]]
 				then
 					echo -ne "\033[A\033[2K\r"
+				elif [[ $repository != 'R' && $repository != 'r' ]]
+				then
+					clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear && return
 				else
-					clear && echo -e "\033[30;41;2;82m--- Error, Entry not recognized ---\033[0m" && sleep 1.5 && clear && continue
+					clear && return
 				fi
 
 				# Enter the Directory where the folder and github repository will reside
@@ -395,11 +484,11 @@ function main {
 					#((counter-=1))
 				#done
 
-				if [[ $DIRECTORY == 'R' || $DIRECTORY == 'r' ]]
+				if [[ $DIRECTORY != 'R' && $DIRECTORY != 'r' ]]
 				then
-					clear && continue
-				else
 					cd && cd $DIRECTORY
+				else
+					clear && return
 				fi
 
 				# Enter the name of the repository to name both the local folder and github repository
@@ -437,7 +526,7 @@ function main {
 
 				else
 					echo -e "\033[30;41;2;82m--- Error, Entry not recognized! ---\033[0m"
-					sleep 1.5 && clear && continue
+					sleep 1.5 && clear && return
 				fi
 				
 				if [ $repository == 'P' ] || [ $repository == 'p' ]
@@ -476,7 +565,7 @@ function main {
 
 						else
 							cd .. && rm -rf $name
-							sleep 1 && clear && continue
+							sleep 1 && clear && return
 						fi
 					else
 						while [[ $(git push -u origin main >/dev/null 2>&1) ]]
@@ -484,7 +573,7 @@ function main {
 							echo -ne "Please, wait...\033[K\r"
 						done
 						sleep 1 && clear && echo -e "\033[30;48;5;82m--- Successful ---\033[0m"
-						sleep 1 && clear && continue
+						sleep 1 && clear && return
 					fi
 
 				elif [ $repository == 'Pr' ] || [ $repository == 'pr' ] || [ $repository == 'PR' ] || [ $repository == 'pR' ]
@@ -522,7 +611,7 @@ function main {
 
 						else
 							cd .. && rm -rf $name
-							sleep 1 && clear && continue
+							sleep 1 && clear && return
 						fi
 					else
 						while [[ $(git push -u origin main >/dev/null 2>&1) ]]
@@ -530,19 +619,19 @@ function main {
 							echo -ne "Please, wait...\033[K\r"
 						done
 						sleep 1 && clear && echo -e "\033[30;48;5;82m--- Successful ---\033[0m"
-						sleep 1 && clear && continue
+						sleep 1 && clear && return
 					fi
 
 				else
 					rm -rf $name
 					echo -e "\033[30;41;2;82m--- Error, Entry not recognized! ---\033[0m"
-					sleep 1.5 && clear && continue	
+					sleep 1.5 && clear && return	
 				fi
 
+}
 
-		# Simple log
-		elif [[ $input == 9 ]]
-		then
+function input_9 {
+
 			if (git log --color | less -R)
 			then
 				clear
@@ -551,10 +640,9 @@ function main {
 				sleep 1 && clear
 			fi
 
+}
 
-		# Delete repository
-		elif [[ $input == 10 ]]
-		then
+function input_10 {
 
 			echo "+========================================================+"
 			echo -e "|                   \033[32;1;82mGit Repo Utility\033[0m                     |"
@@ -572,29 +660,26 @@ function main {
 			read DIRECTORY
 			clear
 
-			if [[ $DIRECTORY == 'R' || $DIRECTORY == 'r' ]]
+			if [[ $DIRECTORY != 'R' && $DIRECTORY != 'r' ]]
 			then
-				clear && continue
-			else
 
 				# Enter the name of the repository to delete
 				echo -n "Enter name of repository: "
 				read RepoToDelete
 				echo -ne "\033[A\033[2K\r"
 
-				if ! (cd && cd $RepoToDelete >/dev/null 2>&1) && ! (cd && cd $DIRECTORY/$RepoToDelete) ;
+				if ! (cd && cd $RepoToDelete >/dev/null 2>&1) && ! (cd && cd $DIRECTORY/$RepoToDelete >/dev/null 2>&1) ;
 				then
 					echo -e "\033[30;41;2;82m--- Error, Repository Not Found! \033[0m"
-					sleep 1.5 && clear && continue	
 				else	
 					if ! (hub delete $RepoToDelete 2>/dev/null)
 					then
+						clear
 						echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
-						sleep 1.5 && clear && continue	
 					else
 						clear && echo -n "Do you want to delete repository's local folder? (Y/n): "
 						read optional
-						echo -ne "\033[A\033[2K\r\033[A\033[2K\r"
+						clear
 						
 						if [[ $optional == 'Y' || $optional == 'y' ]]	
 						then
@@ -607,24 +692,24 @@ function main {
 						fi
 
 						echo -e "\033[30;48;5;82m--- Successful ---\033[0m"
-						sleep 1 && clear && continue 		
 					fi
 				fi
+				sleep 1.5 		
 			fi
+			clear
+}
 
+function input_11 {
 
-		# Detailed log
-		elif [[ $input == 11 ]]
-		then
 			if (git log -p --color | less -R)
 			then
 				clear
 			fi
 
+}
 
-		# Remove all local changes
-		elif [[ $input == 12 ]]
-		then
+function input_12 {		
+	
 			while [[ true ]]
 			do
 				echo -ne "\033[33;1;82mPlease, wait...\033[0m\033[K\r"
@@ -641,10 +726,10 @@ function main {
 				fi
 			done
 
+}
 
-		#Restore file
-		elif [[ $input == 13 ]]
-		then
+function input_13 {
+
 			echo "+========================================================+"
 			echo -e "|                   \033[32;1;82mGit Repo Utility\033[0m                     |"
 			echo "+========================================================+"
@@ -657,7 +742,7 @@ function main {
 			echo -e "|                   \033[31;1;82mEnter [r] to Return\033[0m                  |"
 			echo "+========================================================+"
 			echo -ne "\033[4A\r| \033[36;1;82mEnter file name:\033[0m "
-			read name and echo ""
+			read name
 			clear
 			
 			if [[ $name != 'R' && $name != 'r' ]]
@@ -665,44 +750,53 @@ function main {
 				if (git restore $name >/dev/null 2>&1)
 				then
 					echo -e "\033[30;48;5;82m--- Successful ---\033[0m"
-					sleep 1 && clear && continue
-					break
 				else
 					echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
-					sleep 1 && clear && continue
 				fi
-			else
-				clear && continue
-			fi
-
-
-		# Show changes since last commit
-		elif [[ $input == 14 ]]
-		then
-			if (git diff --color | less -R)
-			then
-
-				if (git diff == "")
-				then
-					echo "--- Repo is up to date ---" && sleep 1.5
-				fi
-				clear
-
-			else
-				echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
 				sleep 1 && clear
+			else
+				clear && return
 			fi
 
+}
 
-		# Quit
-		elif [[ $input == 'Q' || $input == 'q' ]]
-		then
-			echo -e "\033[30;42;5;82m--- Good Bye ---\033[0m" && sleep 1.5 && clear && break
-		else
-			echo -e "\033[30;41;2;82m--- Error, Entry not recognized! ---\033[0m"&& sleep 1.5 && clear && continue	
-		fi
-		
-	done
+function input_14 {
+
+			if ! (git diff --color | less -R) ;
+			then
+				echo -e "\033[30;41;5;82m--- Failed ---\033[0m"
+			fi
+
+}
+
+function input_15 {
+
+			if [[ $(git diff) == '' ]] ;
+			then
+				echo -e "\033[30;48;5;82m--- Repo is up to date ---\033[0m"
+			else
+				echo -e "\033[30;41;5;82m--- Repo is not up to date ---\033[0m"
+			fi
+			sleep 1.5 && clear
+
+}
+
+function input_16 {
+
+			echo "Being developed..." && sleep 1 && clear	
+}
+
+function quit {
+
+			echo -e "\033[30;42;5;82m--- Good Bye ---\033[0m" && sleep 1.5 && clear
+
+}
+
+function error {
+
+			echo -e "\033[30;41;2;82m--- Error, Entry not recognized! ---\033[0m"&& sleep 1.5 && clear
+			return
+
 }
 
 main
